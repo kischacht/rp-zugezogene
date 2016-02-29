e7949f71eac8d04d21b5571543da815008e28cf1
sparksandpatterns <kira.schacht@gmail.com>
2016-02-29 18:25:25 +0100

    Update circle colors and sizes

--- mapscript.js
+++ mapscript.js
@@ -6,22 +6,39 @@ L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Li
 	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
 	maxZoom: 16
 }).addTo(map);
+/*
+function zoomToFeature(e) {
+    map.fitBounds(e.target.getBounds(), zoom = 2);
+}
+*/
 
+var info = L.control();
 //Add circles
+
+var array2 = [];
+
 for (var i = 0; i < array.length; i++) {
       if(i == 121) continue;
-      circle = L.circle([array[i][4], array[i][5]], Math.sqrt(array[i][3])*100, {
-          color: 'red',
-          fillColor: '#f03',
+	innercircle = L.circle([array[i][4], array[i][5]], Math.sqrt(array[i][3])*15, {
+	    color: '#8F8F8D',
+	    fillColor: '#8F8F8D',
+	    fillOpacity: 1
+	}).addTo(map);
+      circle = L.circle([array[i][4], array[i][5]], Math.sqrt(array[i][3])*150, {
+          color: '#fff498',
+          fillColor: '#fff498',
           fillOpacity: 0.5
       }).addTo(map);
       circle.bindPopup(array[i][3] + " Zugezogene")
 	circle.on('mouseover', function (e) {
 	    this.openPopup();
-	    this.setStyle({color: 'orange', fillColor: 'orange'})
+	    this.setStyle({color: '#ffc100', fillColor: '#ffc100'});
 	});
 	circle.on('mouseout', function (e) {
 	    this.closePopup();
-	    this.setStyle({color: 'red', fillColor: '#f03'})
+	    this.setStyle({color: '#fff498', fillColor: '#fff498'});
 	});
+//	circle.on('click', zoomToFeature);
+	array2.push(circle);
 }
+markers.addLayers(array2);
