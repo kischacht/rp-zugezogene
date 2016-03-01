1128a9c77dd6dcb79f741e670e4fcb043849558e
sparksandpatterns <kira.schacht@gmail.com>
2016-03-01 11:13:08 +0100

    Changed colors, added test control bar

--- mapscript.js
+++ mapscript.js
@@ -1,5 +1,9 @@
-//Make map: Esri World Gray Canvas
+////////////////////////////////////////////////////////////
+////////////////////////// Karte ///////////////////////////
+////////////////////////////////////////////////////////////
 
+
+//Karte initialisieren: Esri World Gray Canvas
 var map = L.map('map').setView([array[121][4], array[121][5]], 9);
 
 L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
@@ -12,32 +16,52 @@ function zoomToFeature(e) {
 }
 */
 
-var info = L.control();
-//Add circles
+//Infobar generieren
+var info = L.Control.extend({
+    options: {
+        position: 'topright'
+    },
+
+    onAdd: function (map) {
+        // create the control container with a particular class name
+        var container = L.DomUtil.create('div', 'info');
+	  container.innerHTML = '<p>TestTestTestTestTestTestTest<br>' + array[121][3] + '</p>';
+        return container;
+    }
+});
+map.addControl(new info());
+
+//Kreise hinzufügen
 
 var array2 = [];
 
 for (var i = 0; i < array.length; i++) {
+	//Düsseldorf rauslassen, sonst sieht man nichts
       if(i == 121) continue;
-	innercircle = L.circle([array[i][4], array[i][5]], Math.sqrt(array[i][3])*15, {
-	    color: '#8F8F8D',
-	    fillColor: '#8F8F8D',
-	    fillOpacity: 1
-	}).addTo(map);
+	//Äußerer Kreis relativ zur Anzahl Düsseldorfer
       circle = L.circle([array[i][4], array[i][5]], Math.sqrt(array[i][3])*150, {
-          color: '#fff498',
-          fillColor: '#fff498',
-          fillOpacity: 0.5
+      //    color: '#ffc100',
+          fillColor: '#ffc100',
+          fillOpacity: 0.5,
+	    stroke: false
       }).addTo(map);
       circle.bindPopup(array[i][3] + " Zugezogene")
+	//Innerer grauer Punkt relativ zur Kreisgröße
+	innercircle = L.circle([array[i][4], array[i][5]], Math.sqrt(array[i][3])*25, {
+	    color: '#8F8F8D',
+	    fillColor: '#8F8F8D',
+	    fillOpacity: 1,
+	    stroke: false
+	}).addTo(map);
+	//Hovereffekt: Popup anzeigen, Farbe ändern
 	circle.on('mouseover', function (e) {
 	    this.openPopup();
-	    this.setStyle({color: '#ffc100', fillColor: '#ffc100'});
+	    this.setStyle({color: '#42cac6', fillColor: '#42cac6'});
 	});
 	circle.on('mouseout', function (e) {
 	    this.closePopup();
-	    this.setStyle({color: '#fff498', fillColor: '#fff498'});
-	});
+	    this.setStyle({color: '#ffc100', fillColor: '#ffc100'});
+    });
 //	circle.on('click', zoomToFeature);
 	array2.push(circle);
 }
