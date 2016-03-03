93068f9c6ae388595afb63a4a247addbad44c930
Martin Schurig <martin@schurig.pw>
2016-03-03 18:42:12 +0100

    improve script

--- scripts/run.rb
+++ scripts/run.rb
@@ -6,18 +6,6 @@ require 'uri'
 cities = []
 
 CSV.foreach('../opendata/geburtsort_coded.csv') do |row|
-  # puts "requesting lat and lon for #{row[0]}"
-  # url = URI.encode("https://nominatim.openstreetmap.org/search?format=json&city=#{row[0]}&limit=1")
-  # res = open(url).read
-  # json_res = JSON.parse(res)
-
-  lat = 51
-  lon = 7
-  # if json_res.first
-  #   lat = json_res.first['lat']
-  #   lon = json_res.first['lon']
-  # end
-
   city = {
     type: 'Feature',
     geometry: {
@@ -33,7 +21,7 @@ CSV.foreach('../opendata/geburtsort_coded.csv') do |row|
   }
 
   # adding city object to cities array
-  cities.push(city)
+  cities.push(city) unless $. == 1
 end
 
 data = {type: 'FeatureCollection', features: cities}
