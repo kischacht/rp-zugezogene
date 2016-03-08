de2454409d2235ffb26efee99ee126708b85abf8
sparksandpatterns <kira.schacht@gmail.com>
2016-03-08 13:04:33 +0100

    compute normalized cell values

--- charts/scatterplot/getpop.R
+++ charts/scatterplot/getpop.R
@@ -1,17 +1,29 @@
 library(rgdal)
 library(raster)
 
+#rasterdaten einlesen
 datr <- raster("C:\\Users\\Kira\\OneDrive\\Projekte\\rp-zugezogene\\charts\\scatterplot\\data\\glp00ag15.asc")
 
-check <- read.csv("C:\\Users\\Kira\\OneDrive\\Projekte\\rp-zugezogene\\opendata\\geburtsort_coded.csv")[,c(4,5,7,8)]
+#csv einlesen
+check <- read.csv("C:\\Users\\Kira\\OneDrive\\Projekte\\rp-zugezogene\\opendata\\geburtsort_coded.csv")
 
+#werteaus rasterdaten extrahieren
 #1. long 2. lat
-check$pops <- extract(datr, check[,4:3])
+check$pops <- extract(datr, check[,6:5])
 
+#datensatz auf dopplungen in den rasterzellen prüfen.
+#(nur für den sanity check)
 sorted <- plyr::arrange(check, pops)
-
 sorted$duplicate <- duplicated(sorted$pops)
-
 for(i in 1:nrow(sorted)){
       if(sorted$duplicate[i] == TRUE) sorted$duplicate[i-1] <- TRUE
 }
+
+#distanzen mitteln, personenzahlen addieren
+grouped <- plyr::ddply(check, "pops", plyr::summarise,
+                       distance = mean(Distanz),
+                       countsum = sum(Anzahl))
+
+#orte pro zelle aneinanderreihen
+grouped$orte <- aggregate(Geburtsort_Land~pops,paste, collapse=" | ",
+                    data=check)[2]
